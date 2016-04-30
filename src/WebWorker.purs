96a70c266db435978703dd630ad6bda45a8a8623
FrigoEU <simon.van.casteren@gmail.com>
2016-04-30 10:57:19 +0200

    Fixed effects propagation

--- src/WebWorker.purs
+++ src/WebWorker.purs
@@ -27,16 +27,16 @@ foreign import supportsWebWorkers :: Boolean
 foreign import mkWorker :: forall eff.
                            String 
                            -> Eff (ownsww :: OwnsWW, exception :: EXCEPTION | eff) WebWorker
-foreign import onmessageFromWorker :: forall eff1 eff2. WebWorker 
-                                      -> (MessageEvent -> Eff eff1 Unit)
-                                      -> Eff (ownsww :: OwnsWW | eff2) Unit
+foreign import onmessageFromWorker :: forall eff. WebWorker 
+                                      -> (MessageEvent -> Eff eff Unit)
+                                      -> Eff (ownsww :: OwnsWW | eff) Unit
 foreign import postMessageToWorker :: forall eff. WebWorker 
                                               -> Foreign 
                                               -> Eff (ownsww :: OwnsWW | eff) Unit
 
 foreign import postMessage :: forall eff. Foreign -> Eff (isww :: IsWW | eff) Unit
-foreign import onmessage :: forall eff1 eff2. (MessageEvent -> Eff eff1 Unit)
-                                              -> Eff (isww :: IsWW | eff2) Unit
+foreign import onmessage :: forall eff. (MessageEvent -> Eff eff Unit)
+                                              -> Eff (isww :: IsWW | eff) Unit
 
 type Channels eff = StrMap (Json -> Eff eff Unit)
 
@@ -50,10 +50,10 @@ registerChannel chs (Channel str) handle =
                               handle
                               (decodeJson json)) chs
 
-onmessageC :: forall eff1 eff2. Channels (console :: CONSOLE | eff1) -> Eff (isww :: IsWW | eff2) Unit
+onmessageC :: forall eff. Channels (console :: CONSOLE | eff) -> Eff (isww :: IsWW, console :: CONSOLE | eff) Unit
 onmessageC chs = onmessage (handleMessageWithChannels chs)
 
-onmessageFromWorkerC :: forall eff1 eff2. WebWorker -> Channels (console :: CONSOLE| eff1) -> Eff (ownsww :: OwnsWW | eff2) Unit
+onmessageFromWorkerC :: forall eff. WebWorker -> Channels (console :: CONSOLE | eff) -> Eff (ownsww :: OwnsWW, console :: CONSOLE | eff) Unit
 onmessageFromWorkerC ww chs = onmessageFromWorker ww (handleMessageWithChannels chs)
 
 handleMessageWithChannels :: forall eff. Channels (console :: CONSOLE | eff) -> MessageEvent -> Eff (console :: CONSOLE | eff) Unit
